3298770d8f9f067f54e48e1b98a4a216f8ba54df
Jakub Dundalek <dundalek@gmail.com>
2024-09-28 01:12:05 +0200

    WIP

--- test/io/github/dundalek/theodora/graphviz_test.clj
+++ test/io/github/dundalek/theodora/graphviz_test.clj
@@ -2,13 +2,14 @@
   (:require
    [babashka.fs :as fs]
    [clojure.java.shell :refer [sh]]
+   [clojure.string :as str]
    [clojure.test :refer [deftest is testing]]
    [dorothy.core :as dc]
    [io.github.dundalek.theodora.parser :as parser]
    [io.github.dundalek.theodora.test-utils])
   (:import
-   (java.util.concurrent TimeUnit)
-   (clj_antlr ParseError)))
+   (clj_antlr ParseError)
+   (java.util.concurrent TimeUnit)))
 
 (defn svg [s]
   (:out (sh  "dot" "-Tsvg" :in s)))
@@ -17,15 +18,21 @@
   `(.get (future ~@body) 5 TimeUnit/SECONDS))
 
 (def syntax-error-filenames
-  #{"1676.dot" ; seems like invalid utf8 encoding, antlr does not like it: token recognition error
-    "1411.dot" ; file has syntax error
-    "1308_1.dot" ; file has syntax error
+  #{;; seems like invalid utf8 encoding (graphviz has fallbacks and -Gcharset=latin1 option)
+    ;; antlr does not like it: token recognition error
+    "1676.dot"
+    "share/Latin1.gv"
 
-    "russian.gv" ; would it need to update ranges in grammar?
+    ;; file has syntax error
+    "1411.dot"
+    "1308_1.dot"
+    "share/b545.gv"
+
+    "graphs/russian.gv" ; would it need to update ranges in grammar?
 
     ;; contains two toplevel `digraphs`, graphviz renders only the first one and ignores the second one, we get parser error
     "1845.dot"
-    "multi.gv"})
+    "graphs/multi.gv"})
 
 (def ignored-filenames
   #{"1332.dot" ; looks visually same
@@ -33,46 +40,57 @@
 
     ;; seems to hang/timeout
     "2371.dot"
-    "b100.gv"
-    "b103.gv"
-    "b104.gv"
+    "graphs/b100.gv"
+    "graphs/b103.gv"
+    "graphs/b104.gv"
 
     "2471.dot" ; pretty large graph, not sure what is wrong
     "2239.dot" ; pretty large graph, not sure what is wrong, could it be nested subgraphs?
 
     "2516.dot" ; invalid HTML in attribute
 
-    "html2.gv" ; problem with html label, does not get detected as html `label=<long line 1<BR/>line 2<BR ALIGN="LEFT"/>line 3<BR ALIGN="RIGHT"/>>`
+    "graphs/html2.gv" ; problem with html label, does not get detected as html `label=<long line 1<BR/>line 2<BR ALIGN="LEFT"/>line 3<BR ALIGN="RIGHT"/>>`
 
-    "polypoly.gv" ; numeric ids like`0000`, leading zeroes get stripped results as `0`
-    "root.gv"}) ; `label=07` gets changed to `label=7`
+    "graphs/polypoly.gv" ; numeric ids like`0000`, leading zeroes get stripped results as `0`
+    "share/polypoly.gv"
+    "graphs/root.gv"}) ; `label=07` gets changed to `label=7`
+
+(def graphviz-test-dir "tmp/graphviz/tests/")
+
+(defn path-in-test-dir [path]
+  (str/replace-first (str path) graphviz-test-dir ""))
 
 (def all-test-files
   (->> (concat
-        (fs/glob "." "tmp/graphviz/tests/*.dot")
-        (fs/glob "." "tmp/graphviz/tests/graphs/*.gv"))
-       (sort-by fs/file-name)))
+        (fs/glob graphviz-test-dir "*.dot")
+        ; (fs/glob graphviz-test-dir "**.gv"))
+        (fs/glob graphviz-test-dir "graphs/*.gv")
+        (fs/glob graphviz-test-dir "share/*.gv")
+        (fs/glob graphviz-test-dir "regression_tests/**.gv"))
+       (sort-by path-in-test-dir)))
 
 (def svg-roundtrip-files
   (->> all-test-files
-       (remove (comp (into ignored-filenames syntax-error-filenames) fs/file-name))))
+       (remove (comp (into ignored-filenames syntax-error-filenames) path-in-test-dir))))
 
 (def syntax-error-files
   (->> all-test-files
-       (filter (comp syntax-error-filenames fs/file-name))))
+       (filter (comp syntax-error-filenames path-in-test-dir))))
 
 (deftest syntax-errors
   (doseq [file syntax-error-files]
-    (let [input (slurp (fs/file file))]
-      (testing (str file)
+    (testing (str file)
+      (let [input (slurp (fs/file file))]
         (is (thrown? ParseError (dc/dot (parser/parse input))))))))
 
 (deftest svg-rountrip
   (doseq [file svg-roundtrip-files]
-    (let [input (slurp (fs/file file))]
-      (testing (str file)
-        (is (= (with-timeout (svg input))
-               (with-timeout (svg (dc/dot (parser/parse input))))))))))
+    (testing (str file)
+      (let [input (slurp (fs/file file))]
+        ; (is (= (with-timeout (svg input))
+        ;        (with-timeout (svg (dc/dot (parser/parse input))))))
+        (is (= (svg input)
+               (svg (dc/dot (parser/parse input)))))))))
 
 (comment
   (def file (fs/file "tmp/graphviz/tests/1845.dot"))
@@ -87,15 +105,15 @@
                  :attrs
                  (get "label")))
 
-  ;; mkdir -p tmp/{dot,svg}/{actual,expected}
+  ;; mkdir -p tmp/{dot,svg}/{actual,expected}/{share,graphs,regression_tests}
   (doseq [file svg-roundtrip-files]
     (try
-      (println "Processing" (fs/file-name file))
+      (println "Processing" (path-in-test-dir file))
       (let [input (slurp (fs/file file))
-            filename (str (fs/file-name file) ".svg")
+            filename (str (path-in-test-dir file) ".svg")
             out (dc/dot (parser/parse input))]
-        (spit (str "tmp/dot/expected/" (fs/file-name file)) input)
-        (spit (str "tmp/dot/actual/" (fs/file-name file)) out)
+        (spit (str "tmp/dot/expected/" (path-in-test-dir file)) input)
+        (spit (str "tmp/dot/actual/" (path-in-test-dir file)) out)
         (with-timeout
           (spit (str "tmp/svg/expected/" filename) (svg input)))
         (with-timeout
